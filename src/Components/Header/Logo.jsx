9c664dfd8905467f5ed6fe53c886ec070fcdaef3
chim borey <oceansdtol@gmail.com>
2026-04-29 22:32:07 +0700

    add logo

--- src/Components/Header/Logo.jsx
+++ src/Components/Header/Logo.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import CKC from '../../assets/CKC.jpg'
 const Logo = () => {
   return (
     <Link to={'/'} className=' flex items-center gap-1 cursor-pointer'>
+      <img src={CKC} alt="" className=' w-9 h-9 rounded-full'/>
       <h3 className=' text-red-500 font-bold capitalize text-xl'>World <span className=' text-yellow-500 capitalize'>Travel</span></h3>
     </Link>
   )
